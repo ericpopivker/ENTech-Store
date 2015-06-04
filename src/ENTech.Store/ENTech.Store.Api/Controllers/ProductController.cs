481e972eaf53410cf113d5b09d0d1fa5b9f295ac
raderick <lavatroublebubble@gmail.com>
2015-06-05 02:01:15 +0500

    Cleaning ProductController

--- src/ENTech.Store/ENTech.Store.Api/Controllers/ProductController.cs
+++ src/ENTech.Store/ENTech.Store.Api/Controllers/ProductController.cs
@@ -1,40 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace ENTech.Store.Api.Controllers
 {
 	[RoutePrefix("1.0/store-admin-api/products")]
 	public class ProductController : ApiController
 	{
-		//private IExternalCommandService _externalCommandService;
-
-		//public ProductController(IExternalCommandService externalCommandService)
-		//{
-		//	_externalCommandService = externalCommandService;
-		//}
-
-		[HttpPost]
-		public void Create([FromBody]string value)
-		{
-		}
-
-
-
-		[HttpGet]
-		[Route("{Id:int}")]
-		public string GetById(int id)
-		{
-			return "value";
-		}
-
-
-
-		[HttpGet]
-		[Route("")]
-		public IEnumerable<string> Find()
-		{
-			return new string[] { "value1", "value2" };
-		}
-
 	}
 }
