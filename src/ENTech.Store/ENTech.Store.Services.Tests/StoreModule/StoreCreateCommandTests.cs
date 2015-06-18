87cc70fede557f330d18bf976333dc16696e858d
raderick <lavatroublebubble@gmail.com>
2015-06-18 13:17:02 +0500

    Implementing test for store repository

--- src/ENTech.Store/ENTech.Store.Services.Tests/StoreModule/StoreCreateCommandTests.cs
+++ src/ENTech.Store/ENTech.Store.Services.Tests/StoreModule/StoreCreateCommandTests.cs
@@ -1,4 +1,5 @@
-﻿using ENTech.Store.Entities.UnitOfWork;
+﻿using ENTech.Store.Entities.GeoModule;
+using ENTech.Store.Entities.UnitOfWork;
 using ENTech.Store.Infrastructure.Database;
 using ENTech.Store.Services.StoreModule.Commands;
 using ENTech.Store.Services.StoreModule.Dtos;
@@ -11,23 +12,26 @@ namespace ENTech.Store.Services.Tests.StoreModule
 	public class StoreCreateCommandTests
 	{
 		readonly Mock<IUnitOfWork> _unitOfWorkMock = new Mock<IUnitOfWork>();
-		Mock<IRepository<Entities.StoreModule.Store>> _storeRepositoMock = new Mock<IRepository<Entities.StoreModule.Store>>();
+		readonly Mock<IRepository<Entities.StoreModule.Store>> _storeRepositoryMock = new Mock<IRepository<Entities.StoreModule.Store>>();
 			
 		[Test]
 		public void Execute_When_called_Then_calls_repository_create()
 		{
-			var command = new StoreCreateCommand(_unitOfWorkMock.Object);
+			var command = new StoreCreateCommand(_unitOfWorkMock.Object, 
+				_storeRepositoryMock.Object);
+
 			var request = new StoreCreateRequest
 			{
 				Store = new StoreCreateDto
 				{
-
+					Name = "test store name"
 				}
 			};
 
 			command.Execute(request);
 
-			_storeRepositoMock.Verify(x=>x.Add(It.IsAny<Entities.StoreModule.Store>()), Times.Once);
+			_storeRepositoryMock.Verify(x => x.Add(It.Is<Entities.StoreModule.Store>(y => y.Name == request.Store.Name)),
+				Times.Once);
 		}
 	}
 }
--- src/ENTech.Store/ENTech.Store.Services/StoreModule/Commands/StoreCreateCommand.cs
+++ src/ENTech.Store/ENTech.Store.Services/StoreModule/Commands/StoreCreateCommand.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using ENTech.Store.Entities;
+﻿using ENTech.Store.Entities.GeoModule;
 using ENTech.Store.Entities.UnitOfWork;
+using ENTech.Store.Infrastructure.Database;
 using ENTech.Store.Services.SharedModule.Commands;
 using ENTech.Store.Services.StoreModule.Requests;
 using ENTech.Store.Services.StoreModule.Responses;
@@ -9,13 +9,24 @@ namespace ENTech.Store.Services.StoreModule.Commands
 {
 	public class StoreCreateCommand : DbContextCommandBase<StoreCreateRequest, StoreCreateResponse>
 	{
-		public StoreCreateCommand(IUnitOfWork unitOfWork)
+		private readonly IRepository<Entities.StoreModule.Store> _storeRepository;
+
+		public StoreCreateCommand(IUnitOfWork unitOfWork, 
+			IRepository<Entities.StoreModule.Store> storeRepository)
 			: base(unitOfWork.DbContext, false)
 		{
+			_storeRepository = storeRepository;
 		}
 
 		public override StoreCreateResponse Execute(StoreCreateRequest request)
 		{
+			var storeDto = request.Store;
+
+			_storeRepository.Add(new Entities.StoreModule.Store
+			{
+				Name = storeDto.Name
+			});
+
 			return new StoreCreateResponse
 			{
 				IsSuccess = true
