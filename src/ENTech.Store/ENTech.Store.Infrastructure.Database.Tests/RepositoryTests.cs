1d6b8841a469d03154c2bf06165ab1e2aa54aa38
raderick <lavatroublebubble@gmail.com>
2015-07-06 01:58:34 +0500

    Added tests for GetById for EF6 repository implementation

--- src/ENTech.Store/ENTech.Store.Infrastructure.Database.Tests/RepositoryTests.cs
+++ src/ENTech.Store/ENTech.Store.Infrastructure.Database.Tests/RepositoryTests.cs
@@ -9,15 +9,28 @@ namespace ENTech.Store.Infrastructure.Database.EF6.Tests
 	public class RepositoryTests
 	{
 		private readonly IRepository<StubEntity> _repository;
+
 		private readonly Mock<IDbSetResolver> _dataSetResolver;
+
+		private readonly StubEntity _firstEntity = new StubEntity { Id = 1 };
+		private readonly StubEntity _secondEntity = new StubEntity { Id = 2 };
+		private readonly StubEntity _thirdEntity = new StubEntity { Id = 3 };
+		private readonly StubEntity _fourthEntityWithDupeId = new StubEntity { Id = 4 };
+		private readonly StubEntity _fifthEntityWithDupeId = new StubEntity { Id = 4 };
 
 		public RepositoryTests()
 		{
 			_dataSetResolver = new Mock<IDbSetResolver>();
 			_repository = new Repository<StubEntity>(_dataSetResolver.Object);
 
-
-			var fakeDbSet = new FakeDbSet<StubEntity>(new ObservableCollection<StubEntity>{new StubEntity{Id = 1}});
+			var fakeDbSet = new FakeDbSet<StubEntity>(new ObservableCollection<StubEntity>
+			{
+				_firstEntity,
+				_secondEntity,
+				_thirdEntity,
+				_fourthEntityWithDupeId,
+				_fifthEntityWithDupeId
+			});
 
 			_dataSetResolver.Setup(x => x.Resolve<StubEntity>()).Returns(fakeDbSet);
 		}
@@ -25,9 +38,29 @@ namespace ENTech.Store.Infrastructure.Database.EF6.Tests
 		[Test]
 		public void GetById_When_called_Then_calls_for_dataSetResolver_resolve_to_get_dataset_for_stubEntity()
 		{
-			_repository.GetById(1);
+			var entityId = _firstEntity.Id;
+
+			_repository.GetById(entityId);
 
 			_dataSetResolver.Verify(x=>x.Resolve<StubEntity>());
+		}
+
+		[Test]
+		public void GetById_When_called_Then_gets_item_with_correct_id_from_collecion()
+		{
+			var entityId = _firstEntity.Id;
+
+			var result = _repository.GetById(entityId);
+
+			Assert.AreEqual(_firstEntity, result);
+		}
+
+		[Test]
+		public void GetById_When_called_for_entity_with_dupe_id_Then_does_not_throw()
+		{
+			var entityId = _fourthEntityWithDupeId.Id;
+
+			Assert.DoesNotThrow(()=>_repository.GetById(entityId));
 		}
 
 		public class StubEntity : IEntity
