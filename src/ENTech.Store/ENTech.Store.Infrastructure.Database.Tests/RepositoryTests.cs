3301bd4fb7883a135347227b09293bc32dc1879a
raderick <lavatroublebubble@gmail.com>
2015-07-06 01:28:01 +0500

    Adding first test for Repository<T> and libs to Infrastructure.Database.Tests

---
+++ src/ENTech.Store/ENTech.Store.Infrastructure.Database.Tests/RepositoryTests.cs
@@ -0,0 +1,15 @@
+﻿using ENTech.Store.Infrastructure.Database.Repository;
+using ENTech.Store.Infrastructure.Entities;
+
+namespace ENTech.Store.Infrastructure.Database.Tests
+{
+	public class RepositoryTests
+	{
+		private IRepository<StubEntity> _repository = new Repository<StubEntity>();
+	}
+
+	public class StubEntity : IEntity
+	{
+		public int Id { get; set; }
+	}
+}
---
+++ src/ENTech.Store/ENTech.Store.Infrastructure.Database/Repository/Repository.cs
@@ -0,0 +1,28 @@
+﻿using ENTech.Store.Infrastructure.Entities;
+
+namespace ENTech.Store.Infrastructure.Database.Repository
+{
+	public class Repository<TEntity> : IRepository<TEntity> 
+		where TEntity : IEntity
+	{
+		public void Add(TEntity entity)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public TEntity GetById(int entityId)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Delete(TEntity entity)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Update(TEntity entity)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
