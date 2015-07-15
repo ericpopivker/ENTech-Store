0373de3e842e0753c3c2e213f93b71686d01e661
raderick <lavatroublebubble@gmail.com>
2015-07-16 02:50:40 +0500

    Adding DbEntityStateKeeper to project

---
+++ src/ENTech.Store/ENTech.Store.Infrastructure.Database.EF6/Utility/IDbEntityStateKeeper.cs
@@ -0,0 +1,20 @@
+﻿using ENTech.Store.Infrastructure.Database.QueryExecuter;
+using ENTech.Store.Infrastructure.Entities;
+
+namespace ENTech.Store.Infrastructure.Database.EF6.Utility
+{
+	public interface IDbEntityStateKeeper
+	{
+		void Store<TEntity, TDbEntity>(TEntity entity, TDbEntity dbEntity)
+			where TEntity : IEntity
+			where TDbEntity : IDbEntity;
+
+		TDbEntity Get<TEntity, TDbEntity>(TEntity entity)
+			where TEntity : IEntity
+			where TDbEntity : IDbEntity;
+
+		void Remove<TEntity, TDbEntity>(TEntity entity)
+			where TEntity : IEntity
+			where TDbEntity : IDbEntity;
+	}
+}
