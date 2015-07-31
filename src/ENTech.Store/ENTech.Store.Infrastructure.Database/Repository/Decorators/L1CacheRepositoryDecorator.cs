b78391861fe0cad0952787047ecf63e8fca39eb9
ericpopivker <eric@entechsolutions.com>
2015-07-31 15:30:18 +0200

    Added some missing files

---
+++ src/ENTech.Store/ENTech.Store.Infrastructure.Database/Repository/Decorators/L1CacheRepositoryDecorator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ENTech.Store.Infrastructure.Cache;
+using ENTech.Store.Infrastructure.Entities;
+
+namespace ENTech.Store.Infrastructure.Database.Repository.Decorators
+{
+
+	public class L1CacheRepositoryDecorator<TEntity> : CacheRepositoryDecoratorBase<TEntity>
+		where TEntity : class, IDomainEntity, IAuditable
+	{
+		public L1CacheRepositoryDecorator(IRepository<TEntity> baseRepository, ICache cache) : base(baseRepository, cache)
+		{
+		}
+	}
+
+}
